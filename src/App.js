559580b3ebf67226ee919fa8b6cbe467e5154963
cnewtondev <cnewtondev@gmail.com>
2023-01-09 17:42:16 -0600

    stripe

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
+import React from 'react'
+
 import './App.css';
+import Lottie from "lottie-react";
+import Card from './animations/cardAnimation.json'
+import laptop from './animations/laptop.png'
+
+import { Elements } from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
+import CheckoutForm from './components/CheckoutForm'
+
+const stripePromise = loadStripe('pk_test_51MORuuCyLYwa5YX5LsFZPjJdf9128MQzdtAfzdtpkoPfIF7zVHHluhyAVQojd1j8OoKfdvafjnW1EC8ORCW0Chea00ctfBxfh6');
 
 function App() {
+ 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='left'>
+      
+      Some functions have been disabled
+      
+      <Lottie animationData={Card} loop={true} />
+      </div>
+      <div className='right'>
+      <img
+          src={laptop}
+          alt="laptop"
+          style={{ width: "50%", height: "auto"}}
+        />
+        <div className='cardy'>
+        <Elements stripe={stripePromise}>
+            <CheckoutForm />
+          </Elements>
+        </div>
+      </div>
     </div>
   );
 }
+
+
 
 export default App;
---
+++ src/mytials.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const tials = {
+    perish: 'pk_test_51MORuuCyLYwa5YX5LsFZPjJdf9128MQzdtAfzdtpkoPfIF7zVHHluhyAVQojd1j8OoKfdvafjnW1EC8ORCW0Chea00ctfBxfh6',
+    sec: 'sk_test_51MORuuCyLYwa5YX5qiBeuJJ7c2ZWuN2QULPjdN5GMq2RqGW65a9tDlr1a8lgGVTDcQDkBPeq0b14P76CrIIE7J8G00f7dH6jYE'
+}
+
+export default tials
